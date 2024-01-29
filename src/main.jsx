20e24a38c1e720b8aebd28de70dd7d2e9f025e2c
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-29 02:57:17 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/power-pulse-fs">
+        <BrowserRouter basename="/fs-86-react-node-SaltyUA">
           <App />
         </BrowserRouter>
       </PersistGate>
